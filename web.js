0172dc2926040013d1650237b2820b340f5bf1a6
Nicolas Viano <nicoviano@gmail.com>
2013-07-09 23:06:59 +0000

    reading data from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  var buf =  fs.readFileSync('index.html');
+  var data = buf.toString('utf-8');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
