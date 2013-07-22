fe832ff30fdf648a4137bf881f378fbdcde7c652
Nicolas Viano <nicoviano@gmail.com>
2013-07-22 09:45:24 +0000

    Added information about my specific project about handball matches

--- web.js
+++ web.js
@@ -10,6 +10,14 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
+app.get('/handball_border.png', function(request, response) {
+  var buf =  fs.readFileSync('handball_border.png');
+  //var data = buf.toString('utf-8');
+  response.writeHead(299, {'Content-Type': 'image/png'});
+  response.end(buf, 'binary');
+  //response.send(data);
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
